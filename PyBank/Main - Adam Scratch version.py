98da691b163ef37c67e642bd69ae62ad86c1f37c
unknown <mradamknapp@gmail.com>
2018-02-16 12:34:18 -0600

    --initial

---
+++ PyBank/Main - Adam Scratch version.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 12:26:06 2018
+
+@author: Adam Knapp
+"""
+
+#CODE BELOW is for the count function  (using the text file format)
+
+#
+ Store the file path associated with the file (note the backslash may be OS specific) 
+file = 'desktop/aapl.csv'
+
+# Open the file in "read" mode ('r') and store the contents in the variable "text"
+with open(file, 'r') as text:
+
+    print(text)
+
+    # Store all of the text inside a variable called "lines"
+    lines = text.read()
+
+    # Print the contents of the text file
+    print(lines)
+
+
+# get total number of rows
+    print("Total no. of rows: %d"%(csvreader.line_num))
--- PyBank/main.py
+++ PyBank/main.py
@@ -4,32 +4,7 @@ Created on Thu Feb 15 14:42:30 2018
 
 @author: Adam Knapp
 """
-#Import the File
-import csv
-import os
-import pandas as pd
-import numpy as np
 
-
-csvpath = os.path.join('Desktop\python-challenge\PyBank', 'budget_data_1.csv')
-row_count = sum(1 for row in csv)
-print(row_count)
-
-
-
-
-
-import csv
-import os
-
-csvpath = os.path.join('Desktop\python-challenge\PyBank', 'budget_data_1.csv')
-print(row_count)
-
-
-
-with open('Desktop\python-challenge\PyBank',"r") as f:
-    reader = csv.reader(f,delimiter = ",")
-    data = list(reader)
     row_count = len(data)
     
 import csv
@@ -40,3 +15,47 @@ csvpath = os.path.join('Desktop\python-challenge\PyBank', 'budget_data_1.csv')
 
 
 
+import CSV
+With open(‘budget_data_1.csv’, ‘rb’) as f:
+reader = csv.reader(f)
+for row in reader:
+print row
+
+
+***********************************************************************
+
+# importing csv module
+import csv
+ 
+# csv file name
+filename = "aapl.csv"
+ 
+# initializing the titles and rows list
+fields = []
+rows = []
+ 
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = csvreader.next()
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+ 
+    # get total number of rows
+    print("Total no. of rows: %d"%(csvreader.line_num))
+ 
+# printing the field names
+print('Field names are:' + ', '.join(field for field in fields))
+ 
+#  printing first 5 rows
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+    # parsing each column of a row
+    for col in row:
+        print("%10s"%col),
+    print('\n')
