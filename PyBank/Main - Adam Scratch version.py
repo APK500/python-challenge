1032109efc2c0478494b94eaa25e14969ed2d559
unknown <mradamknapp@gmail.com>
2018-02-16 12:49:27 -0600

    --initial

--- PyBank/Main - Adam Scratch version.py
+++ PyBank/Main - Adam Scratch version.py
@@ -7,8 +7,7 @@ Created on Fri Feb 16 12:26:06 2018
 
 #CODE BELOW is for the count function  (using the text file format)
 
-#
- Store the file path associated with the file (note the backslash may be OS specific) 
+#Store the file path associated with the file (note the backslash may be OS specific) 
 file = 'desktop/aapl.csv'
 
 # Open the file in "read" mode ('r') and store the contents in the variable "text"
@@ -25,3 +24,16 @@ with open(file, 'r') as text:
 
 # get total number of rows
     print("Total no. of rows: %d"%(csvreader.line_num))
+    
+    
+#  ************************************************************************  
+    
+import os
+csvpath = os.path.join('Desktop/python-challenge/PyBank', 'budget_data_1.csv')
+
+
+# # Method 1: Plain Reading of CSVs
+with open(csvpath, 'r') as file_handler:
+    lines = file_handler.read()
+    print(lines)
+    print(type(lines))
