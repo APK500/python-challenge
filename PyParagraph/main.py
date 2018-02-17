9422a94a390478256b66fa94f2b4206869ed64a0
unknown <mradamknapp@gmail.com>
2018-02-16 22:10:49 -0600

    --initial

---
+++ PyParagraph/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 20:22:04 2018
+
+@author: Adam Knapp
+"""
+
+import pandas as pd
+
+
+
+file = open('Desktop/python-challenge/PyParagraph/raw_data/paragraph_1.txt', mode='r')
+
